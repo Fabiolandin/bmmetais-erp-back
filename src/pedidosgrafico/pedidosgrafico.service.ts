bb607e61d930df754915112d981335eafe0d73d0
Fabio Landin <flandin1990@gmail.com>
2026-07-17 09:45:03 -0300

    feat: adicionando comentários de entendimento da sql do findall

--- src/pedidosgrafico/pedidosgrafico.service.ts
+++ src/pedidosgrafico/pedidosgrafico.service.ts
@@ -18,6 +18,14 @@ export class PedidosGraficoService {
       ORDER BY mes ASC
     `;
 
+    //strfime conversor de sqlite, em outros dbs muda esse comando de formatação
+    //inner join : junta a tabela pedido com itempedido
+    //count distinc : conta os pedidos unicos pq um pedido tem vários itens pedidos e o join cria várias linhas
+    //cast... as real : converte o resultado pra número de ponto flutuante.
+    //sum: soma os valores de cada item * preço
+    //group by: agrupando as linhas por ,es
+    //order by mes asc: ordena cronologicamente do mais antigo para o mais novo
+
     return (result as any[]).map(item => ({
       mes: item.mes,
       total_pedidos: Number(item.total_pedidos),
