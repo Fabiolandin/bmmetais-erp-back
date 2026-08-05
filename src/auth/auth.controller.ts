f05fe7f40422a5ee6df0266f88753c5e3eafbe08
Fabio Landin <flandin1990@gmail.com>
2026-08-05 11:21:50 -0300

    feat: adicionando decorator public na rota login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './login.dto';
+import { Public } from './public.decorator';
 
 @ApiTags('Autenticação')
 @Controller('auth')
@@ -9,6 +10,7 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('login')
+    @Public()
     @ApiOperation({ summary: 'Autenticar funcionário e gerar token JWT '})
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto.email, loginDto.senha);
