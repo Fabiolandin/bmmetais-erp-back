f7fbbcf84f024476faeab3edb8fab2fd45435a92
Fabio Landin <flandin1990@gmail.com>
2026-08-11 16:48:59 -0300

    feat: comentários para melhor entendimento de fluxo

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -3,21 +3,24 @@ import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
-export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+export class RolesGuard implements CanActivate { //can activate é um guard puro do Nest que retorna true ou false
+  constructor(private reflector: Reflector) {} //injeta o reflector, a ferramenta que sabe ler os metadados anexados por decorators
 
+  //antes de deixar rodar o comportamento original do guard rodar ele checa se a rota tem o isPublic
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [ //getAllAndOverride busca um metadado especifico
       context.getHandler(),
       context.getClass(),
     ]);
 
+    //Se a rota n tiver roles libera.
     if (!requiredRoles || requiredRoles.length === 0) {
       return true;
     }
 
     const { user } = context.switchToHttp().getRequest();
 
+    //compara se o a role do decorator e do user que extraiu do banco são iguais
     return requiredRoles.includes(user?.role);
   }
 }
